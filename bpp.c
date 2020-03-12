19d66c561a9defeab4626cea3c459697efb3d2a7
Leonardo de Abreu <leonardo.abreu@lia.ufc.br>
2020-03-12 17:31:55 -0300

    read_instance

--- bpp.c
+++ bpp.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+int n =0;
 
-int read_instance(char * path, int * C, int * w, int * wSum){
+int read_instance(char * path, int * C, int ** w, int * wSum){
 	FILE* file;
 	fopen_s(&file, path, "r");
 	if (file == NULL) {
 		printf("Arquivo nao existente:\npath: %s\n", path);
 		return -1;
 	}
-	int n;
 	fscanf_s(file, "%d%d", &n, C);
-	w = (int *) malloc(n*sizeof(int));
+	*w = (int *) malloc(n*sizeof(int));
 
 	*wSum = 0;
 	for (int i = 0; i < n; ++i) {
-		fscanf_s(file, "%d", &w[i]);
-		*wSum += w[i];
+		fscanf_s(file, "%d", &(*w)[i]);
+		*wSum += (*w)[i];
 	}
 	fclose(file);
 
-	printf("C: %d\n"\
-			"wSum: %d\n",*C,*wSum);
-	printf("[ ");
-	for (int i = 0; i < n; ++i) {
-		printf("%d ", w[i]);
-	}
-	printf("]");
 	return 1;
 
 }
@@ -34,7 +27,14 @@ int main(int argc, char *argv[]){
 	int C, *w, wSum;
 	char path[] = "D:\\\\Codigos\\metaheuristics\\instances\\Hard28\\Hard28_BPP14.txt";
 	
-	read_instance(path,&C,w,&wSum);
+	read_instance(path,&C,&w,&wSum);
 
+	printf("C: %d\n"\
+			"wSum: %d\n",C,wSum);
+	printf("[ ");
+	for (int i = 0; i < n; ++i) {
+		printf("%d ", w[i]);
+	}
+	printf("]");
 	return 0;
 }
