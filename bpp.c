b2ac014ec1fb91e3a65e74602323dd58ddeea8ac
Leonardo de Abreu <leonardo.abreu@lia.ufc.br>
2020-03-13 13:59:32 -0300

    bbp_t struct

--- bpp.c
+++ bpp.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-int n =0;
 
-int read_instance(char * path, int * C, int ** w, int * wSum){
+/*! \struct bpp_t
+ *  \brief Instance of Bin Packing Problem
+ *
+ *  Guarda uma instancia do BBP, ou seja, a capaciade máxima do pacote, os pesos dos itens,
+ *  o numero de itens e a soma total de pesos.
+ */
+typedef struct bpp_t {
+	int C, w_sum;
+	int * w;
+	size_t n;
+} bpp;
+
+int read_instance(char * path, bpp * instance){
 	FILE* file;
 	fopen_s(&file, path, "r");
 	if (file == NULL) {
 		printf("Arquivo nao existente:\npath: %s\n", path);
 		return -1;
 	}
-	fscanf_s(file, "%d%d", &n, C);
-	*w = (int *) malloc(n*sizeof(int));
+	fscanf_s(file, "%d%d", &instance->n, &instance->C);
+	instance->w = (int *) malloc(instance->n*sizeof(int));
 
-	*wSum = 0;
-	for (int i = 0; i < n; ++i) {
-		fscanf_s(file, "%d", &(*w)[i]);
-		*wSum += (*w)[i];
+	instance->w_sum = 0;
+	for (int i = 0; i < instance->n; ++i) {
+		fscanf_s(file, "%d", &(instance->w)[i]);
+		instance->w_sum += (instance->w)[i];
 	}
 	fclose(file);
 
@@ -24,16 +35,17 @@ int read_instance(char * path, int * C, int ** w, int * wSum){
 }
 
 int main(int argc, char *argv[]){
-	int C, *w, wSum;
+	// int C, *w, wSum;
+	bpp inst;
 	char path[] = "D:\\\\Codigos\\metaheuristics\\instances\\Hard28\\Hard28_BPP14.txt";
 	
-	read_instance(path,&C,&w,&wSum);
+	read_instance(path, &inst);
 
 	printf("C: %d\n"\
-			"wSum: %d\n",C,wSum);
+			"wSum: %d\n",inst.C,inst.w_sum);
 	printf("[ ");
-	for (int i = 0; i < n; ++i) {
-		printf("%d ", w[i]);
+	for (int i = 0; i < inst.n; ++i) {
+		printf("%d ", inst.w[i]);
 	}
 	printf("]");
 	return 0;
